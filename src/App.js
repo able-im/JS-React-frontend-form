dbe89f0e4c9fb8790e1ce05f7f7086b6aa6eb135
able-im <artemuhov@gmail.com>
2022-08-21 17:13:23 +0200

    Style of login form
    Added CSS style for login form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
 
-const FormExample = () => (
+const FormLogin = () => (
   <Segment inverted>
     <Form inverted>
-      <Form.Group widths='equal'>
+      <Form.Group className="login_form_group" widths='equal'>
         <Form.Input fluid label='User name:' placeholder='login' />
         <Form.Input fluid type="password" label='Password:' placeholder='password' />
+        <Button type='submit'>Login</Button>
       </Form.Group>
-      <Button type='submit'>Login</Button>
-    </Form>
+      </Form>
   </Segment>
 )
-export default FormExample
+export default FormLogin
