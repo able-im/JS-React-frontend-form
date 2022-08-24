795dfc04abc4e3de066b01fe5355cc08ac7d3a2d
able-im <artemuhov@gmail.com>
2022-08-24 21:02:58 +0200

    Redirect to contacts page on submit
    Make redirection for test case when pass and login is = 123

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
 
-const FormLogin = () => (
-  <Segment inverted>
-    <Form inverted>
-      <Form.Group className="login_form_group" widths='equal'>
-        <Form.Input fluid label='User name:' placeholder='login' />
-        <Form.Input fluid type="password" label='Password:' placeholder='password' />
-        <Button type='submit'>Login</Button>
-      </Form.Group>
-      </Form>
-  </Segment>
-)
-export default FormLogin
+class FormExampleCaptureValues extends Component {
+  state = { name: '', password: '', submittedName: '', submittedPassword: '' }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    const { name, password } = this.state
+
+    this.setState({ submittedName: name, submittedPassword: password })
+  }
+
+  render() {
+    const { name, password, submittedName, submittedPassword } = this.state;
+    if(submittedName === '123' && submittedPassword === '123') {
+	window.location.replace("http://localhost:3000/contact"); };
+
+    return (
+      <div>
+	<Segment inverted>
+        <Form inverted onSubmit={this.handleSubmit}>
+          <Form.Group className="login_form_group" widths='equal'>
+            <Form.Input fluid label='User name:'
+              placeholder='Login'
+              name='name'
+              onChange={this.handleChange}
+              value={name}
+            />
+            <Form.Input fluid label='Password:' 
+              type="password"           
+              placeholder='password'
+              name='password'
+              onChange={this.handleChange}
+              value={password}
+            />
+            <Button content='Login' />
+          </Form.Group>
+        </Form>
+        </Segment>
+      </div> 
+    )
+  }
+}
+
+export default FormExampleCaptureValues
