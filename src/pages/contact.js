d2c2efd02b16040982d61f0a02deb81f511458e4
able-im <artemuhov@gmail.com>
2022-08-26 21:07:02 +0200

    Added "Add contact" function

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,130 +1,127 @@
 import { useState } from 'react'
-import { renderIntoDocument } from 'react-dom/test-utils';
+import { Icon } from 'semantic-ui-react'
 
 const data = [
-  {
-    employeeId: 1,
-    name: 'John Doe',
-    email: 'johndoe@email.com',
-    position: 'Frontend Developer',
-    phone: '773123456',
-  },
-  {
-    employeeId: 2,
-    name: 'Sara',
-    email: 'sara@email.com',
-    position: 'HR Executive',
-    phone: '334123456',
-  },
-  {
-    employeeId: 3,
-    name: 'Mike',
-    email: 'mike@email.com',
-    position: 'Backend Developer',
-    phone: '554123456',
-  },
-  {
-    employeeId: 4,
-    name: '',
-    email: '',
-    position: '',
-    phone: '',
-  },
+	{
+		employeeId: 1,
+		name: 'John Doe',
+		email: 'johndoe@email.com',
+		position: 'Frontend Developer',
+		phone: '773123456',
+	},
+	{
+		employeeId: 2,
+		name: 'Sara',
+		email: 'sara@email.com',
+		position: 'HR Executive',
+		phone: '334123456',
+	},
+	{
+		employeeId: 3,
+		name: 'Mike',
+		email: 'mike@email.com',
+		position: 'Backend Developer',
+		phone: '554123456',
+	},
 ]
-
-function AddContact(e) {
-
-  const newContact =
-  {
-    employeeId: data.length + 1,
-    name: '',
-    email: '',
-    position: '',
-    phone: '',
-  }
-
-  data.push(newContact);
-  console.log(data);
-  // here i have to re-render table
-}
 
 
 const EditableTable = () => {
-  const [employeeData, setEmployeeData] = useState(data)
 
-  const onChangeInput = (e, employeeId) => {
-    const { name, value } = e.target
+	const [employeeData, setEmployeeData] = useState(data)
 
-    const editData = employeeData.map((item) =>
-      item.employeeId === employeeId && name ? { ...item, [name]: value } : item
-    )
+	const onChangeInput = (e, employeeId) => {
+		const { name, value } = e.target
 
-    setEmployeeData(editData)
-  }
+		const editData = employeeData.map((item) =>
+			item.employeeId === employeeId && name ? { ...item, [name]: value } : item
+		)
+		setEmployeeData(editData)
+	}
 
-  return (
-    <div className="container">
-      <h1 className="title">Contacts</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Position</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employeeData.map(({ employeeId, name, email, position, phone }) => (
-            <tr key={employeeId}>
-              <td>
-                <input
-                  name="name"
-                  value={name}
-                  type="text"
-                  onChange={(e) => onChangeInput(e, employeeId)}
-                  placeholder="Type Name"
-                />
-              </td>
-              <td>
-                <input
-                  name="email"
-                  value={email}
-                  type="text"
-                  onChange={(e) => onChangeInput(e, employeeId)}
-                  placeholder="Type Email"
-                />
-              </td>
-              <td>
-                <input
-                  name="position"
-                  type="text"
-                  value={position}
-                  onChange={(e) => onChangeInput(e, employeeId)}
-                  placeholder="Type Position"
-                />
-              </td>
-              <td>
-                <input
-                  name="phone"
-                  type="text"
-                  value={phone}
-                  onChange={(e) => onChangeInput(e, employeeId)}
-                  placeholder="Type Phone"
-                />
-              </td>
+	const AddContact = (e) => {
+		const newContact =
+		{
+			employeeId: employeeData.length + 1,
+			name: '',
+			email: '',
+			position: '',
+			phone: '',
+		}
+		employeeData.push(newContact)
+		setEmployeeData([...employeeData])
+		console.log(employeeData)
+	}
 
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <button onClick={(e) => AddContact(e)}>
-          Add
-        </button>
-      </div>
-    </div >
-  )
+	return (
+		<div className="container">
+			<h1 className="title">Contacts</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Position</th>
+						<th>Phone</th>
+					</tr>
+				</thead>
+				<tbody>
+					{employeeData.map(({ employeeId, name, email, position, phone }) => (
+						<tr key={employeeId}>
+							<td>
+								<input
+									name="name"
+									value={name}
+									type="text"
+									onChange={(e) => onChangeInput(e, employeeId)}
+									placeholder="Type Name"
+								/>
+							</td>
+							<td>
+								<input
+									name="email"
+									value={email}
+									type="text"
+									onChange={(e) => onChangeInput(e, employeeId)}
+									placeholder="Type Email"
+								/>
+							</td>
+							<td>
+								<input
+									name="position"
+									type="text"
+									value={position}
+									onChange={(e) => onChangeInput(e, employeeId)}
+									placeholder="Type Position"
+								/>
+							</td>
+							<td>
+								<input
+									name="phone"
+									type="text"
+									value={phone}
+									onChange={(e) => onChangeInput(e, employeeId)}
+									placeholder="Type Phone"
+								/>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<div>
+				<button className="add_button"
+					floated='left'
+					onClick={(e) => AddContact(e)}
+					icon
+					labelPosition='left'
+					primary
+					size='small'>
+					<Icon name='user' />
+					Add new
+				</button>
+			</div>
+		</div >
+	)
 }
 
 export default EditableTable
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,68 +2,68 @@ import React, { Component } from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
 
 class FormCaptureValues extends Component {
-  state = { name: '', password: '', submittedName: '', submittedPassword: '', isLoading: true, users: [], error: null }
+	state = { name: '', password: '', submittedName: '', submittedPassword: '', isLoading: true, users: [], error: null }
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value })
-  handleSubmit = () => {
-    const { name, password } = this.state
-    this.setState({ submittedName: name, submittedPassword: password })
-  }
+	handleChange = (e, { name, value }) => this.setState({ [name]: value })
+	handleSubmit = () => {
+		const { name, password } = this.state
+		this.setState({ submittedName: name, submittedPassword: password })
+	}
 
-  getFetchUsers() {
-    this.setState({
-      loading: true
-    }, () => {
-      fetch("http://localhost:8000/users").then(res => res.json()).then(result => this.setState({
-        loading: false,
-        users: result
-      })).catch(console.log);
-    });
-  }
-  componentDidMount() {
-    this.getFetchUsers();
-  }
+	getFetchUsers() {
+		this.setState({
+			loading: true
+		}, () => {
+			fetch("http://localhost:8000/users").then(res => res.json()).then(result => this.setState({
+				loading: false,
+				users: result
+			})).catch(console.log);
+		});
+	}
+	componentDidMount() {
+		this.getFetchUsers();
+	}
 
-  render() {
-    const { users, error, name, password, submittedName, submittedPassword } = this.state;
+	render() {
+		const { users, error, name, password, submittedName, submittedPassword } = this.state;
 
-    users.map(user => {
-      const {
-        name,
-        password
-      } = user;
-      if (submittedName === name && submittedPassword === password) {
-        window.location.replace("http://localhost:3000/contact");
-      };
-    })
+		users.map(user => {
+			const {
+				name,
+				password
+			} = user;
+			if (submittedName === name && submittedPassword === password) {
+				window.location.replace("http://localhost:3000/contact");
+			};
+		})
 
 
-    return (
-      <div>
-        <Segment inverted>
-          <Form inverted onSubmit={this.handleSubmit}>
-            <Form.Group className="login_form_group" widths='equal'>
-              <Form.Input fluid label='User name:'
-                placeholder='Login'
-                name='name'
-                onChange={this.handleChange}
-                value={name}
-              />
-              <Form.Input fluid label='Password:'
-                type="password"
-                placeholder='password'
-                name='password'
-                onChange={this.handleChange}
-                value={password}
-              />
-              <Button content='Login' />
-            </Form.Group>
-          </Form>
-        </Segment>
-      </div>
+		return (
+			<div>
+				<Segment inverted>
+					<Form inverted onSubmit={this.handleSubmit}>
+						<Form.Group className="login_form_group" widths='equal'>
+							<Form.Input fluid label='User name:'
+								placeholder='Login'
+								name='name'
+								onChange={this.handleChange}
+								value={name}
+							/>
+							<Form.Input fluid label='Password:'
+								type="password"
+								placeholder='password'
+								name='password'
+								onChange={this.handleChange}
+								value={password}
+							/>
+							<Button content='Login' />
+						</Form.Group>
+					</Form>
+				</Segment>
+			</div>
 
-    )
-  }
+		)
+	}
 }
 
 export default FormCaptureValues
